63d292313e6d9228ea7eb4d85b58f066fe404b21
suneel-nadipalli <140884446+suneel-nadipalli@users.noreply.github.com>
2024-04-27 09:22:57 +0530

    Delete static directory

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.btn').addEventListener('click', function() {
-    document.body.classList.add('pixelize-out');
-    // Optionally, redirect to the new page after a delay
-    setTimeout(function() {
-        window.location.href = '/story-options'; // Change '/new-page' to the URL of the new page
-    }, 1000); // Adjust the delay to match the duration of the animation
-});
